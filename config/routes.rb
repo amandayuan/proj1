bf29a9724070e4a8a9b5a0956afaa0d1d5db07a1
Amanda Yuan <amandayuan@berkeley.edu>
2017-10-27 19:06:16 -0700

    part4

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :trainers
   patch '/capture', to:'pokemon#capture', as: :capture
+  patch "/damage", to:"pokemon#damage", as: :damage
 end
